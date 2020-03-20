e3fc8007c2b645bb5b3f526fe5f88e451b427802
Gwen <gwendou95@hotmail.fr>
2020-03-20 17:02:08 +0100

    :fire: DELETE ALL

--- models/Task.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const taskSchema = mongoose.Schema({
-    name: String,
-    isFinish: Boolean,
-  },{ collection: 'task' })
-
-module.exports = mongoose.model('task', taskSchema)
