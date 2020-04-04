4d21cc7688021647ba8e020df06c9246bb9b69c3
Gwen <gwendou95@hotmail.fr>
2020-04-04 14:28:42 +0200

    :fire: Removing tests

--- index.js
+++ index.js
@@ -5,18 +5,7 @@ const routes = require('./routes');
 const initDbConnection = require('./dao/connection-manager');
 const app = express();
 
-const daos = require('./dao');
-const UserDAO = daos.UserDAO;
-
 initDbConnection();
-// routes(app);
-
-
-const users = UserDAO.getAllUsers();
-console.log(JSON.stringify(users));
-
-
-
-
+routes(app);
 
 app.listen(process.env.API_PORT, () => console.log(`BurgerAPI Started on port ${process.env.API_PORT}...`));
