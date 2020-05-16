1edd66371332df7800ae5e97909d97627721b958
Pothier Lucas <barder60>
2020-05-16 17:22:38 +0200

    :fire: remove useless function in core controller

--- controllers/core.controller.js
+++ controllers/core.controller.js
@@ -1,20 +1,6 @@
 const mongoose = require('mongoose')
 class Core {
     // -------------------------
-    // Prend les paramètres de recherche et les traite
-    // pour qu'ils soient compréhensibles pour mongoDB
-    // -------------------------
-    static paramize(params) {
-        return params
-    }
-    // -------------------------
-    // Vérifie les données passées en paramètre pour
-    // contrôler qu'elles correspondent bien au model du service
-    // -------------------------
-    static checkData(data) {
-        return data
-    }
-    // -------------------------
     // Prend une liste de models en paramètre et
     // applique si nécessaire les populate ou autre
     // opération de cosmétiques sur les données
@@ -23,6 +9,7 @@ class Core {
         const isAlone = !Array.isArray(list);
         // On s'arrange pour traiter une liste
         if (isAlone) list = [list];
+        console.log('avant return')
         return (
             Promise.resolve()
                 .then(() => {
@@ -41,6 +28,8 @@ class Core {
                     if (options.populates) {
                         return this.getModel().populate(list, options.populates)
                     }
+                    console.log(list);
+                    console.log('toto');
                     return list
                 })
                 // On retourne le résultat attendu
@@ -51,72 +40,15 @@ class Core {
     // Lance une recherche sur les documents
     // -------------------------
     static find(search, options = {}) {
-        // On récupère la limite si il y en a une
-        let limit = options.limit || 300
-        if (!options.force && limit > 300) limit = 300;
         return (
             Promise.resolve()
                 .then(() => this.cleanModelData(search))
-                // On construit l'objet de recherche
-                .then(s => this.paramize(s))
                 .then(s =>
                     this.getModel()
                         .find(s)
-                        // On limite toujours le nombre de résultats
-                        .limit(limit)
-                        .sort(options.order || '-created_at')
+                        .sort(options.order || 'created_at')
                         .exec()
                 )
-        )
-    }
-    // -------------------------
-    // Retourne le render d'un document
-    // Note : "id" peut être un ID ou un
-    // objet de recherche
-    // -------------------------
-    static read(id, options = {}) {
-        return Promise.resolve()
-            .then(() =>
-                typeof id === 'object'
-                    ? this.getModel()
-                        .findOne(id)
-                        .exec()
-                    : this.getModel()
-                        .findById(id)
-                        .exec()
-            )
-            .then(model => {
-                if (!model) {
-                    throw new Error(`Unknow item ${id}`)
-                }
-                return this.render(model, options)
-            })
-    }
-    // -------------------------
-    // Supprime un ou des documents
-    // -------------------------
-    static remove(id) {
-        return Promise.resolve().then(() =>
-            typeof id === 'object'
-                ? // On lance la suppression des documents
-                this.getModel()
-                    // On recherche la liste à supprimer
-                    .find(id, '_id')
-                    .then(list =>
-                        Promise.all([
-                            // On récupère les ID des documents
-                            list.map(l => l.get('id')),
-                            // On supprime avec la recherche
-                            this.getModel().deleteMany(id),
-                        ])
-                    )
-                    // On retourne la liste des ID supprimés
-                    .then(([ids]) => ids)
-                : // On supprime le document
-                this.getModel()
-                    .findOneAndDelete({ _id: id })
-                    .exec()
-                    .then(() => id)
         )
     }
 
@@ -141,9 +73,6 @@ class Core {
                                 .findById(id)
                                 .exec(),
                     ])
-                )
-                .then(([cleanedData, model]) =>
-                    Promise.all([model, this.checkData(cleanedData)])
                 )
                 .then(([model, checkedData]) => {
                     if (!model)
@@ -173,8 +102,6 @@ class Core {
                 .then(() => this.filterFields(data, options.authorizedFields))
                 // On nettoie les données
                 .then(filteredData => this.cleanModelData(filteredData))
-                // On vérifie les données
-                .then(cleanedData => this.checkData(cleanedData))
                 // Création du document
                 .then(checkedData => this.getModel().create(checkedData))
         )
@@ -192,8 +119,6 @@ class Core {
                 )
                 // On nettoie les données
                 .then(filteredList => filteredList.map(this.cleanModelData.bind(this)))
-                // On vérifie les données
-                .then(cleanedList => cleanedList.map(this.checkData.bind(this)))
                 // Création du document
                 .then(checkedList => this.getModel().insertMany(checkedList))
         )
@@ -202,35 +127,13 @@ class Core {
     // Nettoie les données passées en paramètre
     // -------------------------
     static cleanModelData(data) {
-        const result = {}
-        const emptyValues = ['', null, 'null', undefined]
-        const castValues = [
-            // Si on trouve un texte "true" ou
-            // "false", on le cast en Boolean
-            [/true|false/i, value => value === 'true'],
-        ]
+        const result = {};
+        const emptyValues = ['', null, 'null', undefined];
         Object.keys(data).forEach(key => {
             // On exclue les valeurs vides
-            if (emptyValues.includes(data[key])) return
-            // On gère les chaines de caractères dans la recherche
-            if (typeof data[key] === 'string') {
-                // On transforme les données qui en ont besoin
-                castValues.find(regs => {
-                    // On récupère le cast
-                    const cast = regs.pop()
-                    // On parcours chaque regex pour voir si il y en a une qui match
-                    const reg = regs.find(r => r.test(data[key]))
-                    if (reg) {
-                        // On a trouvé une correspondance, on cast
-                        data[key] = cast(data[key].trim())
-                        // On s'arrête là
-                        return true
-                    }
-                    return false
-                })
-            }
+            if (emptyValues.includes(data[key])) return;
             // On sauvegarde les données dans l'objet
-            result[key] = data[key]
+            result[key] = data[key];
         });
 
         return result
@@ -240,33 +143,15 @@ class Core {
     // ne garder que les champs listés
     // -------------------------
     static filterFields(data, fields) {
-        if (!fields) return data
-        const isRemoveMode = fields.find(f => f.startsWith('-'))
+        if (!fields) return data;
         // On construit un nouvel objet qui ne
         // va contenir que les données finale
-        let filteredFields = {}
-        if (isRemoveMode) {
-            filteredFields = data
-            fields = fields.map(f => f.replace(/^\-/i, ''))
-        }
+        let filteredFields = {};
         // On parcours les champs autorisés
         fields.forEach(key => {
-            if (isRemoveMode) delete filteredFields[key]
-            else if (data[key] !== undefined) filteredFields[key] = data[key]
-        })
+            if (data[key] !== undefined) filteredFields[key] = data[key]
+        });
         return filteredFields
-    }
-    // -------------------------
-    // Transforme une chaine de caractères
-    // comportant des séparateurs en tableau
-    // -------------------------
-    static splitString(value, sep, cast = String) {
-        if (typeof value === 'number') return [value]
-        if (value.indexOf(sep) >= 0) {
-            // Il y a plusieurs valeurs, on les transforme en tableau
-            return value.split(sep).map(val => cast(val.trim()))
-        }
-        return [value]
     }
     // -------------------------
     // Retourne le model mongoose associé
