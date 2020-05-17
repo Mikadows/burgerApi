b8bbdfa51fb4833c5e1691f4aa5863418096b798
Gwen <gwendou95@hotmail.fr>
2020-05-17 17:56:46 +0200

    :sparkles: Add a middleware and comments

--- dao/user.dao.js
+++ dao/user.dao.js
@@ -52,13 +52,27 @@ class UserDao {
     }
 
     /**
-     *
+     * Check if the user id is admin
      * @param id
      * @returns {boolean}
      */
     static async isAdmin(id){
         let UserExist = await UserDao.find({$and:[{type:{$eq:"admin"}},{_id: id}]});
         if(Array.isArray(UserExist) && UserExist.length) return true;
+        else return false;
+    }
+
+    /**
+     * Check if the user id is preparer or is admin
+     * @param id
+     * @returns {boolean}
+     */
+    static async isPreparerOrAdmin(id){
+        const user = await UserDao.find({$or:[
+                                                        {$and:[{type:{$eq:"admin"}},{_id: id}]},
+                                                        {$and:[{type:{$eq:"preparer"}},{_id: id}]}
+                                                    ]});
+        if(Array.isArray(user) && user.length) return true;
         else return false;
     }
 
