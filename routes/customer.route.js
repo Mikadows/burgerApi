d0e5cbcbcf97b51582777f5f0d56e5f88598475b
Gwen <gwendou95@hotmail.fr>
2020-05-17 17:10:46 +0200

    :pencil: Add comments

--- routes/customer.route.js
+++ routes/customer.route.js
@@ -8,14 +8,30 @@ const MenuController = Controllers.MenuController;
 
 module.exports = function(app) {
 
+    /**
+     * Customer routes
+     */
+
+    /**
+     * Make an order
+     */
     app.post('/customer/order', bodyParser.json(), OrderController.create_order);
 
+    /**
+     * Products access routes
+     */
     app.get('/products', ProductsController.products_get_all);
     app.get('/product/:productId', ProductsController.get_product_by_id);
 
+    /**
+     * Menus access routes
+     */
     app.get('/menus', MenuController.menus_get_all);
     app.get('/menu/:menuId', MenuController.get_menu_by_id);
 
+    /**
+     * Promotions access routes
+     */
     app.get('/promotions', bodyParser.json(), PromotionController.getAll);
     app.get('/promotion/:promotionId', PromotionController.getPromotionById);
 
